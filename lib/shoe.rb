2f0a244c350e1ed5ff884d37fba25e90b71deae3
Annia Forney <anniaforney@gmail.com>
2019-04-24 15:01:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,16 +3,11 @@ class Shoe
   attr_reader :brand 
 
   def initialize(brand)
-    @brand = brand
+    @brands = brands
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-def brands
-  all.brands = "new"
-  puts "only keeps track of unique brands"
   end
 end
